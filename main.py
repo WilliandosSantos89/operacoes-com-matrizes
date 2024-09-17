b2a6e8c68797e4cec2e0cd331d69339883aefe98
Willian dos Santos <48397346+WilliandosSantos89@users.noreply.github.com>
2024-09-17 20:29:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,3 +15,69 @@ def criar_matriz():
 
     return np.array(matriz)
 
+def operacao_elemento(matriz, operacao):
+    valor = float(input("Digite o valor para realizar a operação: "))
+
+    if operacao == "somar":
+        return matriz + valor
+    elif operacao == "subtrair":
+        return matriz - valor
+    elif operacao == "multiplicar":
+        return matriz * valor
+    elif operacao == "dividir":
+        return matriz / valor
+    else:
+        print("Operação inválida.")
+        return matriz
+    
+def operacao_soma_total(matriz, operacao):
+    soma_total = matriz.sum()
+    valor = float(input("Digite o valor para realizar a operação: "))
+
+    if operacao == "somar":
+        return soma_total + valor
+    elif operacao == "subtrair":
+        return soma_total - valor
+    elif operacao == "multiplicar":
+        return soma_total * valor
+    elif operacao == "dividir":
+        return soma_total / valor
+    else:
+        print("Operação inválida.")
+        return soma_total
+    
+def menu():
+    print("\nMenu de Operações: ")
+    print("1. Criar uma matriz")
+    print("2. Operações em cada elemento da matriz")
+    print("3. Operações com a soma total da matriz")
+    print("4. Exibir matriz")
+    print("5; Sair")
+
+def main():
+    matriz = None
+
+    while True:
+        menu()
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == "1":
+            natriz = criar_matriz()
+            print("Matriz criada com sucesso")
+
+        elif opcao == "2":
+            if matriz is None:
+                print("Crie uma matriz primeiro. ")
+            else:
+                print("\nOperacões em cada elemento: ")
+                print("a. Somar")
+                print("b. Subtrair")
+                print("c. Multiplicar")
+                print("d. Dividir")
+                oper = input("Escolha a operação (a/bc/d): ").strip().lower()
+
+            
+
+
+
+    
